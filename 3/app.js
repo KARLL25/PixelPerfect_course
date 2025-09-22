d9bba4fd138344c02224a4d6171c6c0134148734
daiki <danilka.markelov2@mail.ru>
2025-09-22 19:11:38 +0800

    third

---
+++ 3/app.js
@@ -0,0 +1,36 @@
+// Number
+// bigint
+// String
+// Boolean
+// Undefined 
+// Null
+// Symbol 
+// Object
+
+const age = 22;
+console.log(typeof age);
+
+const bigInt = 182738291379812738n;
+console.log(typeof bigInt);
+
+const firstName = 'Name';
+console.log(typeof firstName);
+
+let age2;
+console.log(typeof age2);
+
+const obj = {
+    name: 'George',
+    age: 28,
+    isWorked: true
+}
+
+
+const num = 10;
+const num2 = 5;
+
+console.log(num + num2);
+console.log(num - num2);
+console.log(num * num2);
+console.log(num / num2);
+console.log(num % num2);
