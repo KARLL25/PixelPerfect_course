d1ff422c44c6271fd4146a64520e48a8eab6a31b
daiki <danilka.markelov2@mail.ru>
2025-09-22 19:49:53 +0800

    1

---
+++ lesson_03/app_03.js
@@ -0,0 +1,10 @@
+let titleProject = 'Оценка стоимости';
+let screenValue = 'шаблонные, с уникальным дизайном, с анимациями';
+
+let screenPrice = 10000;
+let percentage = 10;
+let fullPrice = 100000;
+let responsive = true;
+
+console.log(titleProject, screenValue, screenPrice, percentage, fullPrice, responsive);
+
